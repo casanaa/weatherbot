9ff0f3a29107371d24319760f57abf29847c5687
casanaa <antoniocasana@yahoo.com>
2023-10-16 18:33:19 -0500

    working on index & style

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+const apiKey = "0ae0ef9a70a305f19a10c6944c863c30";
+let cityArray = []
+
+//use moment to display date in a normal format 
+const TodayIs = moment().format('MMMM Do , YYYY');
+    $('#todayIs').text(TodayIs);
